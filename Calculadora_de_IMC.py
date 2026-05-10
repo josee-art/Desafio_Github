208f3c7d0107a2a6bd2e4979c8d74353e6941ba5
José Luiz <jose.e@ufpi.edu.br>
2026-05-10 18:38:56 -0300

    Adicionado o arquivo do código em Python!

---
+++ Calculadora_de_IMC.py
@@ -0,0 +1,11 @@
+print("\nCalculadora de IMC - Índice de Massa Corporal\n")
+print("Escolha uma das opções: ")
+print("1 - Cadastrar paciente ")
+print("2 - IMC dos pacientes cadastrados\n")
+opcao = int(input("Digite o número da opção que deseja: "))
+if opcao == 1:
+    print("\nVocê escolheu 1!")
+elif opcao == 2:
+    print("\nVocê escolheu 2!")
+else:
+    print("Escolha uma opção válida!")
