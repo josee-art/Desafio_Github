9dd1a38adbb03e4ca99323c53aa523b7bf8c567c
José Luiz <jose.e@ufpi.edu.br>
2026-05-12 22:41:00 -0300

    Adicionada função de carregar e de salvar dados

--- Calculadora_de_IMC.py
+++ Calculadora_de_IMC.py
@@ -1,12 +1,44 @@
+import os
+
 pacientes = []
+
+DIRETORIO_ATUAL = os.path.dirname(os.path.abspath(__file__))
+CAMINHO_ARQUIVO = os.path.join(DIRETORIO_ATUAL, "pacientes.txt")
 
 def Menu(): 
     print("\nCalculadora de IMC - Índice de Massa Corporal\n")
     print("Escolha uma das opções: ")
     print("1 - Cadastrar paciente ")
-    print("2 - IMC dos pacientes cadastrados\n")
-    opcao = int(input("Digite o número da opção que deseja: "))
-    return opcao
+    print("2 - IMC dos pacientes cadastrados")
+    print("3 - Salvar dados")
+    print("4 - Sair do programa\n")
+    try:
+        opcao = int(input("Digite o número da opção que deseja: "))
+        return opcao
+    except ValueError:
+        return 0
+    
+def Carregar_Dados():
+    if not os.path.exists(CAMINHO_ARQUIVO):
+        return
+
+    with open(CAMINHO_ARQUIVO, "r", encoding="utf-8") as arquivo:
+        for linha in arquivo:
+            dados = linha.strip().split(" | ")
+
+            nome = dados[0]
+            peso = float(dados[1])
+            altura = float(dados[2])
+
+            paciente = {
+                "nome": nome,
+                "peso": peso,
+                "altura": altura
+            }
+
+            pacientes.append(paciente)
+
+    print("\n[SUCESSO] Dados carregados!")
 
 def Cadastro():
     print("\n====Cadastro de Pacientes====\n")
@@ -48,25 +80,61 @@ def Mostrar_pacientes():
         print(f"IMC: {imc:.2f}")
 
         if imc < 18.5:
-            print("Classificação: Abaixo do peso\n")
+            print("Classificação: Abaixo do peso")
+            print("================================")
 
         elif imc < 25:
-            print("Classificação: Peso normal\n")
+            print("Classificação: Peso normal")
+            print("================================")
 
         elif imc < 30:
-            print("Classificação: Sobrepeso\n")
+            print("Classificação: Sobrepeso")
+            print("================================")
         else:
-            print("Classificação: Obesidade\n")
+            print("Classificação: Obesidade")
+            print("================================")
 
+def Salvar_Dados():
+
+    with open(CAMINHO_ARQUIVO, "w", encoding="utf-8") as arquivo:
+        linha = f"Dados dos pacientes (Nome, Peso, Altura, IMC):\n"
+        arquivo.write(linha + "\n")
+        for paciente in pacientes:
+            imc = Calculo_IMC(paciente['peso'], paciente['altura'])
+            if imc < 18.5:
+             abaixo = str("Classificação: Abaixo do peso")
+             linha = f"--> {paciente['nome']} | {paciente['peso']} | {paciente['altura']} | {abaixo}\n"
+             arquivo.write(linha)
+
+            elif imc < 25:
+             normal = str("Classificação: Peso normal")
+             linha = f"--> {paciente['nome']} | {paciente['peso']} | {paciente['altura']} | {normal}\n"
+             arquivo.write(linha)
+
+            elif imc < 30:
+             sobrepeso = str("Classificação: Sobrepeso")
+             linha = f"--> {paciente['nome']} | {paciente['peso']} | {paciente['altura']} | {sobrepeso}\n"
+             arquivo.write(linha)
+
+            elif imc >= 30:
+             obesidade = str("Classificação: Obesidade")
+             linha = f"--> {paciente['nome']} | {paciente['peso']} | {paciente['altura']} | {obesidade}\n"
+             arquivo.write(linha)
+
+    print(f"\n[SUCESSO] Dados gravados em: {CAMINHO_ARQUIVO}")
+
+Carregar_Dados()
 while True:
     opcao = Menu()
     if opcao == 1:
         Cadastro()
     elif opcao == 2:
         Mostrar_pacientes()
+    elif opcao == 3:
+        Salvar_Dados()
+    elif opcao == 4:
+        print("\nEncerrando o sistema de IMC. Até logo!")
+        break
     else:
-        print("Escolha uma opção válida!")
-
-
-
+        print("\nEscolha uma opção válida!")
 
